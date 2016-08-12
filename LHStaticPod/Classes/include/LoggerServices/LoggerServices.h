976bd3a611c40d93c3ae9bdf10b35c28492bffb0
Harsh Jaiswal <harsh.jaiswal@actolap.com>
2016-08-12 13:54:55 +0530

    Logger Files Added

---
+++ LHStaticPod/Classes/include/LoggerServices/LoggerServices.h
@@ -0,0 +1,15 @@
+//
+//  LoggerServices.h
+//  LoggerServices
+//
+//  Created by Harsh Jaiswal on 12/08/16.
+//  Copyright © 2016 Harsh Jaiswal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LoggerServices : NSObject
+
++ (void)logData:(NSString *)logString;
+
+@end
